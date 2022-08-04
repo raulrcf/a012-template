cfe0be03f6f895d3e06d29d4ae8f3bd8cb3b0fef
Raul Cesar <raulcesar.rcf@gmail.com>
2022-08-03 21:07:23 -0300

    atividade

---
+++ script.js
@@ -0,0 +1,20 @@
+/*const idade = 15
+
+if(idade >=60 ||idade >=16 && idade <= 17){
+    console.log("Titulo Facultativo")
+}else if(idade >= 18 ){
+    console.log("Tem direito ao titulo de eleitor ")
+}else{console.log("Voce nao tem direito ao titulo de eleitor")}
+*/
+
+const media = 8
+
+
+if(media>=5 && media<=10){
+    if(media>10){console.log("Numero invalido")}
+    console.log("Aprovado")}
+else if (media >= 3 && media<=4){
+    console.log("Recuperacao")
+}else if (media < 2){
+    console.log("Reprovado")
+}else {console.log("Dado invalido")}
